505364efeface7d696e1612dced6e6f8f9d9a8ca
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2020-12-22 01:21:12 -1000

    Finished all the backend.
    Add a page to see the database, a page dedicated to errors + better internal error handling.

--- run.py
+++ run.py
@@ -2,26 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import common
+import flask
+from flask import Flask, render_template, request
 
+common.flaskInstance = app = Flask(__name__)
 common.fileOpened = False
-
-#Simple shortcut
-def ws():
-    """
-    Just print our comma! Shortcut function!
-
-    Returns
-    -------
-    None.
-
-    """
-    
-    common.csvDataBase.write(str(","))
 
 #Function to write patterns
 def writeValues(text="", diff="0", c1="0", c2="1"):
     """
-    Write data in the way we need to generate a correct database
+    Write data in the way we need to generate a correct CSV database, updated in the file at end of func.
 
     Parameters
     ----------
@@ -40,23 +30,103 @@ def writeValues(text="", diff="0", c1="0", c2="1"):
 
     """
     
-    common.csvDataBase.write(str(text))
-    ws()
-    common.csvDataBase.write(str(diff))
-    ws()
-    common.csvDataBase.write(str(c1))
-    ws()
-    common.csvDataBase.write(str(c2))
-    common.csvDataBase.write("\n")
+    common.csvDataBase.write(str(text)+","+str(diff)+","+str(c1)+","+str(c2)+",\n")
+    common.csvDataBase.flush()
 
-from flask import Flask, render_template, request
+def htmlifiedTable(txt):
+    sources = txt.split("\n")
+    i = 0
+    output = ""
+    
+    while i<len(sources):
+        #Use two-times parsing for simplification and better HTML
+        if (sources[i] != ""):
+            l1 = -1
+            l2 = -1
+            d = -1
+            t = ""
+            values = sources[i].split(",")
+            length = len(values)
+            #First parsing
+            if (length >= 0):
+                t = values[0]
+            if (length >= 1):
+                if (values[1].isnumeric()):
+                    d = int(values[1])
+            if (length >= 2):
+                if (values[2].isnumeric()):
+                    l1 = int(values[2])
+            if (length >= 3):
+                if (values[3].isnumeric()):
+                    l2 = int(values[3])
+            
+            #Second parsing, HTML embedding
+            output += "<tr>"
+            output += "<td>"
+            if t != "":
+                output += t
+            else:
+                output += "inconnu"
+            output += "</td>"
+            output += "<td>"
+            if d != -1:
+                output += str(d)
+            else:
+                output += "inconnu"
+            output += "</td>"
+            output += "<td>"
+            if l1 != -1:
+                output += str(l1)
+            else:
+                output += "inconnu"
+            output += "</td>"
+            output += "<td>"
+            if l2 != -1:
+                output += str(l2)
+            else:
+                output += "inconnu"
+            output += "</td>"
+            output += "</tr>"
+        i+=1
+    return output
 
-common.flaskInstance = app = Flask(__name__)
+@app.errorhandler(Exception)
+def server_error(err):
+    """
+    Default error handler to retrieve proper error WP with right redirections.
+    By using <path>§<description>, the redirection will not be history backward but redirection to <path>, and the error string <description>
+    If "§" isn't used, all the error is displayed as the error string.
+
+    Parameters
+    ----------
+    err : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+    int
+        DESCRIPTION.
+
+    """
+    app.logger.exception(err)
+    replace = "window.location.href = &quot;"
+    if ("§" in str(err)):
+        val = str(err).split("§")[0]
+        if (val == "go_back"):
+            replace = "window.history.go(-1); return false;"
+        else:
+            replace += val+"&quot;"
+        err = str(err).split("§")[1]
+    else:
+        replace += "/&quot;"
+    return render_template("errors.html", error_string=str(err), redirection=replace), 500
 
 @app.route('/')
 def index():
     """
-    Main page, used to enter the file you want to open
+    Main page, used to enter the file you want to open. If something fails with the files, redirections should be made to here, and let user choose a new file or try again.
 
     Returns
     -------
@@ -69,7 +139,7 @@ def index():
 @app.route('/form', methods = ['GET', 'POST'])#GET is used to handle add words for 2=<
 def form():
     """
-    WP that shows the text you want to add to the DB.
+    WP that shows the form used to add words to the DB.
 
     Returns
     -------
@@ -78,21 +148,20 @@ def form():
 
     """
     
-    if (common.fileOpened == False):
-        fp = request.form.get("fp")
-        try:
-            common.csvDataBase = open(str(fp), "a+")
-        except IOError:
-            print("Failed to open the file!")
-            return render_template('index.html', errorType="IOError: Erreur, fichier non ouvert.")
+    if ("common.csvDataBase" in globals()):
+        if (common.fileOpened == False or common.csvDataBase.closed == True):
+            fp = request.form.get("fp")
+            try:
+                common.csvDataBase = open(str(fp), "a+")
+            except IOError:
+                    flask.abort(603, description=("/§Impossible to open file as write only: "+str(fp)))
+            else:
+                common.fileOpened = True
+                return render_template('form.html')
         else:
-            print("Succeed to open the file!")
-            common.fileOpened = True
-            print("\n--- Opened file. ---")
-            print("File name:", common.csvDataBase.name)
             return render_template('form.html')
     else:
-        return render_template('form.html')
+        flask.abort(600, description="/§File not opened.")
     
 
 @app.route('/resultat', methods=['POST'])
@@ -106,18 +175,20 @@ def resultat():
         DESCRIPTION.
 
     """
-    
-    if (common.fileOpened == True):
-        result=request.form
-        writeValues(result.get("mot"), result.get("difficulte"), result.get("lettre1"), result.get("lettre2"))
-        return render_template('resultat.html', mot=result.get("mot"), difficulte=result.get("difficulte"), lettre1=result.get("lettre1"), lettre2=result.get("lettre2"))
+    if ("common.csvDataBase" in globals()):
+        if (common.fileOpened == True and common.csvDataBase.closed == False):
+            result=request.form
+            writeValues(result.get("mot"), result.get("difficulte"), result.get("lettre1"), result.get("lettre2"))
+            return render_template('resultat.html', mot=result.get("mot"), difficulte=result.get("difficulte"), lettre1=result.get("lettre1"), lettre2=result.get("lettre2"))
+        else:
+            flask.abort(601, description=("/§File closed unexpectedly: "+str(common.csvDataBase.name)))
     else:
-        return render_template('index.html', errorType="Erreur, fichier non ouvert.")
+        flask.abort(600, description="/§File not opened.")
 
 @app.route('/table', methods=['GET'])
 def table():
     """
-    Shows all the data we have in the DB as a WP.
+    Shows all the data we have in the DB as a WP by using an HTML table
 
     Returns
     -------
@@ -125,8 +196,18 @@ def table():
         DESCRIPTION.
 
     """
-    
-    return render_template('table.html')
+    file = ""
+    if ("common.csvDataBase" in globals()):
+        try:
+            file = open(common.csvDataBase.name, 'r')
+        except IOError:
+            flask.abort(602, description=("/§Impossible to open file as read only: "+str(common.csvDataBase.name)))
+        else:
+            data = file.read();
+            file.close()
+            return render_template('table.html', data=htmlifiedTable(data))
+    else:
+        flask.abort(600, description="/§File not opened.")
 
 print("\n--- Server started. ---")
 app.run(debug=True)
